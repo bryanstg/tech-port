cf524510f07281ef85ec198b5578565607d5dd2a
Bryan García <bryan1997st@gmail.com>
2021-09-18 00:29:44 -0500

    Create conditional for welcome

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Welcome } from "../component/Welcome.jsx";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [openModal, setOpenModal] = useState(true);
+	return (
+		<div className="home">
+			{openModal ? (
+				<Welcome modal={openModal} setModal={setOpenModal} />
+			) : (
+				<React.Fragment>
+					<h1>Hello Rigo!</h1>
+					<p>
+						<img src={rigoImage} />
+					</p>
+					<a href="#" className="btn btn-success">
+						If you see this green button, bootstrap is working
+					</a>
+				</React.Fragment>
+			)}
+		</div>
+	);
+};
