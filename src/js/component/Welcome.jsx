8112f1a743957783218c36ea6f4245ac20005191
Bryan García <bryan1997st@gmail.com>
2021-09-23 08:36:49 -0500

    Closed strings into template literals and updated the intro

--- src/js/component/Welcome.jsx
+++ src/js/component/Welcome.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { NasaLogo } from "./../svg/NasaLogo.jsx";
 
 export const Welcome = ({ modal, setModal }) => {
 	return (
 		<div className="welcome-box">
 			<div className="welcome">
-				<h2 className="welcome__title">Hi! Welcome to Tech Port | NASA</h2>
+				<h2 className="welcome__title">{`Welcome to Tech Port | NASA`}</h2>
 				<p className="welcome__text">
-					This is not the official website of the Tech Port by NASA. This is a refactory web project made with
-					the public{" "}
+					{`This is not the official website of the Tech Port by NASA. This is a rebuild web project made with
+					the public `}
 					<a
 						href="https://api.nasa.gov/"
 						target="_blank"
 						rel="noopener noreferrer"
 						className="welcome__text--a">
-						NASA API
+						{"NASA API"}
 					</a>
-					. If you want to visit the original website, please{" "}
+					{". If you want to visit the original website, please "}
 					<a
 						href="https://techport.nasa.gov/home"
 						target="_blank"
@@ -26,17 +27,20 @@ export const Welcome = ({ modal, setModal }) => {
 					</a>
 				</p>
 				<p className="welcome__text">
-					Made with ❤ by{" "}
+					{"Made with ❤ by "}
 					<a
 						href="https://www.linkedin.com/in/bryan-garcia-fullstack/"
 						target="_blank"
 						rel="noopener noreferrer"
-						className="welcome__text--a">
-						Bryan García
+						className="welcome__text--a welcome__bryan">
+						{"Bryan García"}
 					</a>
 					.
 				</p>
-				<button onClick={event => setModal(false)}>{`Let's go`}</button>
+				<button className="welcome__button" onClick={event => setModal(false)}>{`Let's go`}</button>
+				<div className="welcome__nasa-logo">
+					<NasaLogo />
+				</div>
 			</div>
 		</div>
 	);
