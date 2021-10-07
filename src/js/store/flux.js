a5f42c666f21106c0f5f5fc6e3d6a258582bc2ec
Bryan García <bryan1997st@gmail.com>
2021-10-07 00:00:46 -0500

    Added most viewed actions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -7,43 +7,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 			apiUri: apiUri,
 			techPortUrl: "https://techport.nasa.gov/",
 			projects: [],
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			mostViewedProjectsId: [93128, 14656, 32946, 14653, 13075, 94282, 93175, 95868, 94431, 94206],
+			mostViewedProjects: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
 			getAllProjects: async () => {
 				//Get all the projects availables
 				const store = getStore();
@@ -55,11 +22,12 @@ const getState = ({ getStore, getActions, setStore }) => {
 						const body = await response.json();
 						let filteredProjects = [];
 						for (let project of body.projects) {
-							const projectYear = project.lastUpdated.split("-");
-							if (projectYear[1] > 8 && projectYear[0] >= 2021) {
+							//Make sure that online keep 20 projects in store
+							const amountOfProjects = 40;
+							if (filteredProjects.length >= amountOfProjects) continue;
+							const projectLastUpdated = project.lastUpdated.split("-");
+							if (projectLastUpdated[1] > 8 && projectLastUpdated[0] >= 2021) {
 								filteredProjects.push(project);
-							} else {
-								continue;
 							}
 						}
 						setStore({
@@ -73,9 +41,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			getProject: async id => {
 				//Get a specific project by id
+				try {
+					const store = getStore();
+					const response = await fetch(`${store.apiUri}/projects/${id}?api_key=${apiKey}`);
+					if (response.ok) {
+						const project = await response.json();
+						return project;
+					}
+				} catch (error) {
+					throw new Error(`Something happened trying to fetch project ${id}: ${error}`);
+				}
+			},
+			getMostViewedProjects: async () => {
 				const store = getStore();
-				const response = await fetch(`${store.apiUri}/projects/${id}`);
-				const project = await response.json();
+				const actions = getActions();
+				const mostViewedResponses = await Promise.all(
+					store.mostViewedProjectsId.map(async projectId => {
+						const body = await actions.getProject(projectId);
+						const projectInfo = {
+							projectId: body.project.projectId,
+							title: body.project.title,
+							description: body.project.description
+						};
+						return projectInfo;
+					})
+				);
+				setStore({
+					...store,
+					mostViewedProjects: mostViewedResponses
+				});
 			}
 		}
 	};
